b5f115b744d6bb108d52ab166a541bd1cebfbc04
zypeh <zypeh.geek@gmail.com>
2021-01-03 15:47:29 +0800

    Oh my, a brand new project

---
+++ app/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import Parser
+
+import Protolude
+
+main :: IO ()
+main = putByteString "a"
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,86 +1,209 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Parser where
 
+import Protolude hiding (try, some)
+import GHC.Base (String)
+import Data.Char
+import Data.Maybe (fromJust)
+
+import qualified Data.ByteString as BS
 import Text.Megaparsec
 import qualified Text.Megaparsec.Byte as B
 import qualified Text.Megaparsec.Byte.Lexer as L
 
-import Data.Char (isAlphaNum, chr)
-import Data.ByteString.Internal (ByteString, c2w)
-import Data.ByteString (pack)
-import GHC.Word (Word8)
-import Data.Void (Void)
+import Data.ByteString.Internal (c2w, w2c)
 
-import Term
-import Name (Name, toName)
+data Identifier
+  = Hole
+  | Id ByteString 
+  | UnderscoreId ByteString  -- Id starts with an underscore, eg "_a"
+  deriving (Show)
+
+data IntType
+  = I8 | I16 | I32 | I64
+  | U8 | U16 | U32 | U64
+  deriving (Show)
+
+data FloatType = F32 | F64 deriving (Show)
+
+data Ty
+  = TyInt IntType
+  | TyFloat FloatType
+  | TyTuple [Ty]
+  | TyArray Ty -- (Maybe Int)
+  | TyFunction { arg :: Ty, ret :: Ty }
+  deriving (Show)
+  -- sum type
+  -- record type
+  -- Polytype?
+
+data Declaration
+  = DeclFunctionBind { name :: Identifier, args :: [Identifier], expr :: Expr }
+  | DeclValBind Identifier Expr
+  | DeclTyBind Identifier Ty
+  deriving (Show)
 
 type Parser = Parsec Void ByteString
 
 ws :: Parser ()
 ws = L.space B.space1 (L.skipLineComment "--") (L.skipBlockComment "{-" "}-")
 
-lexeme = L.lexeme ws -- so I no need to append `<* spaces` everywhere
-symbol s = lexeme (B.string s)
+lexeme = L.lexeme ws
+symbol = lexeme . B.string
 char c = lexeme (B.char . c2w $ c)
-parens p = char '(' *> p <* char ')'
-braced p = char '{' *> p <* char '}'
+parens = between (char '(') (char ')')
+braced = between (char '{') (char '}')
+brackets = between (char '[') (char ']')
+space = char ' '
 
--- | Convert a byte to char.
+pStringLiteral :: Parser ByteString
+pStringLiteral = BS.pack <$> (char '\"' *> manyTill anySingle (char '\"'))
+
+-- | Convert a byte to char
 toChar :: Word8 -> Char
 toChar = chr . fromIntegral
 {-# INLINE toChar #-}
 
-keyword :: ByteString -> Bool
-keyword x = x == "let" || x == "in" || x == "forall"
+pIdentifier :: Parser Identifier
+pIdentifier = ident >>= validate
+  where
+    ident = do
+      x <- takeWhile1P Nothing (isAlphaNum . toChar)
+      x <$ ws
 
-pIdentifier :: Parser Name
-pIdentifier = do
-  x <- takeWhile1P Nothing (isAlphaNum . toChar)
-  toName x <$ ws
+    validate x = pure $ case (BS.length x, w2c $ BS.head x) of
+      (1, '_') -> Hole
+      (_, '_') -> UnderscoreId x
+      (_,   _) -> Id x
 
-pBind :: Parser Name
-pBind = pIdentifier <|> (toName <$> symbol "_")
-
-pString :: Parser ByteString
-pString = pack <$> (char '\"' *> manyTill anySingle (char '\"'))
-
-pNumber :: Parser Int
-pNumber = lexeme L.decimal
-
-pAtomicLit :: Parser Lit
-pAtomicLit = choice [ LInt <$> pNumber
-                   , LBool True <$ symbol "true"
-                   , LBool False <$ symbol "false"
-                   , LString <$> pString
-                   ]
-
-atomicTerm :: Parser Term
-atomicTerm = (Var <$> pIdentifier)
-         <|> (Lit <$> pAtomicLit)
-         <|> parens atomicTerm
-
-typeParse :: Parser Type
-typeParse = choice
-  [ tInt <$ symbol "Int"
-  , tBool <$ symbol "Bool"
+pPrimTy :: Parser Ty
+pPrimTy = choice
+  [ TyInt I8  <$ "i8"
+  , TyInt I16 <$ "i16"
+  , TyInt I32 <$ "i32"
+  , TyInt I64 <$ "i64"
+  , TyInt U8  <$ "u8"
+  , TyInt U16 <$ "u16"
+  , TyInt U32 <$ "u32"
+  , TyInt U64 <$ "u64"
+  , TyFloat F32 <$ "f32"
+  , TyFloat F64 <$ "f64"
+  , tupleParse pTy >>= \ts -> case unwrapSingleTuple ts of
+      Left ty -> pure ty
+      Right t -> pure $ TyTuple t
+  , TyArray <$> brackets pPrimTy
+  , parens pTy
   ]
 
-bindingWithTypeAnnotation :: Parser (Name, Type)
-bindingWithTypeAnnotation = parens ((,) <$> pBind <*> (char ':' *> typeParse))
+tupleParse p = parens $ sepBy1 p (char ',')
+arrayParse p = brackets $ sepBy1 p (char ',')
 
-pLambda :: Parser Term
-pLambda = do
-  char '\\'
-  bindingsWType <- some bindingWithTypeAnnotation
-  char '.'
-  term <- atomicTerm
-  pure $ foldr (\(name, ty) term -> Lam name ty term) term bindingsWType
+unwrapSingleTuple :: [a] -> Either a [a]
+unwrapSingleTuple xs = case length xs of
+  1 -> Left . fromJust $ headMay xs
+  _ -> Right xs
 
--- entrypoint
-termParser :: Parser Term
-termParser = ws *> pLambda <* eof
+pTy :: Parser Ty
+pTy = pPrimTy `chainl1` arrow
+  where
+    -- should write a tokenizer so I don't need to deal with spaces anymore
+    arrow = ws *> symbol "->" $> TyFunction
 
-parseSrc :: String -> ByteString -> Either String Term
-parseSrc filename src = case parse termParser filename src of
-  Left e -> Left $ errorBundlePretty e
-  Right t -> Right t
+chainl1 :: Parser a -> Parser (a -> a -> a) -> Parser a
+p `chainl1` op = do { a <- p; rest a }
+  where
+    rest a = (do f <- op
+                 b <- p
+                 rest (f a b))
+             <|> return a
+
+pTypeBind :: Parser Declaration
+pTypeBind = do
+  name <- pIdentifier
+  DeclTyBind name <$> (char ':' *> pTy)
+
+data Literal
+  = IntNumber Int
+  | FloatNumber Double
+  | Boolean Bool
+  deriving (Show)
+
+pLiteral :: Parser Literal
+pLiteral = choice
+  [ IntNumber <$> L.decimal
+  , FloatNumber <$> L.float
+  , Boolean False <$ symbol "false"
+  , Boolean True <$ symbol "true"
+  ]
+
+data Atom
+  = AtomLit Literal
+  | AtomStr ByteString
+  | AtomList [Atom]
+  | AtomTuple [Atom]
+  deriving (Show)
+
+pAtom :: Parser Atom
+pAtom = choice
+  [ AtomLit <$> pLiteral
+  , AtomStr <$> pStringLiteral
+  , AtomList <$> arrayParse pAtom
+  , tupleParse pAtom >>= \as -> case unwrapSingleTuple as of
+    Left a -> pure a
+    Right a -> pure $ AtomTuple a
+  , parens pAtom
+  ]
+
+data Expr
+  = ExprAtom Atom
+  | ExprApp Expr Expr
+  | ExprLet { _pat :: Identifier, _expr :: Expr, _body :: Expr } -- no type annotation ?
+  -- lambdas where ?
+  -- if then else ?
+  deriving (Show)
+
+pLet :: Parser Expr
+pLet = do
+  symbol "let"
+  x <- pIdentifier -- should be pattern binder
+  symbol "="
+  t <- pExpr
+  symbol "in"
+  u <- pExpr
+  pure $ ExprLet x t u
+
+pApp :: Parser Expr
+pApp = do
+  fn <- pExpr
+  args <- some pExpr
+  pure (foldr ExprApp fn args)
+
+pExpr :: Parser Expr
+pExpr = choice
+  [ ExprAtom <$> pAtom
+  , pApp
+  , pLet
+  ]
+
+pFuncBind :: Parser Declaration
+pFuncBind = do
+  name <- pIdentifier
+  args <- some pIdentifier
+  symbol "="
+  expr <- pExpr
+  pure $ DeclFunctionBind name args expr
+
+pValBind :: Parser Declaration
+pValBind = do
+  name <- pIdentifier
+  symbol "="
+  expr <- pExpr
+  pure $ DeclValBind name expr
+
+pDecl :: Parser Declaration
+pDecl = try pValBind <|> try pFuncBind <|> pTypeBind
+
+parseSource :: String -> ByteString -> Either String Declaration
+parseSource filename src =
+  case parse pDecl filename src of
+    Left e -> Left $ errorBundlePretty e
+    Right decl -> Right decl
