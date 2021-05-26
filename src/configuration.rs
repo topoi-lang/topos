879d26f03abba7d548e513e695d0f6c721862035
zypeh <zypeh.geek@gmail.com>
2021-05-26 15:37:14 +0800

    timing of the function

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,19 @@
 use env_logger::Builder;
-use log::LevelFilter;
+use std::time::Instant;
+use std::time::Duration;
+
+///! ## Configuration layer
+///! most of the configuration arguments are from the environment
 
 pub fn init_log() {
     let mut builder = Builder::from_env("LOG");
     builder.init();
+}
+
+/// Used to calculate a execution time of a function
+pub fn timing<T>(body: impl FnOnce() -> T) -> (T, Duration) {
+    let start = Instant::now();
+    let result = body();
+    let end = Instant::now();
+    (result, end - start)
 }
