9bdcc4d603fc8e28a65e95097791b8cec15db2f6
zypeh <zypeh.geek@gmail.com>
2021-06-11 00:51:27 +0800

    WIP: parser processTerm_

---
+++ src/Parser.hs
@@ -0,0 +1,81 @@
+{-# language DeriveGeneric #-}
+{-# language DeriveAnyClass #-}
+{-# language DerivingStrategies #-}
+{-# language OverloadedStrings #-}
+
+module Parser where
+
+import GHC.Generics (Generic)
+import Z.Data.Text (Text, Print)
+import Z.Data.Vector.FlatMap (FlatMap)
+
+import qualified Z.Data.Vector.FlatMap as M
+import qualified Z.Data.Builder as B
+
+import Tokeniser
+    ( Atom (Ident, Int, String, Nil)
+    , Term (Atom, List)
+    )
+
+type Name = Text
+type Scope = FlatMap Name Expr
+
+-- SKI combinator
+data Expr
+    = Var Name
+    | Lit Atom
+    | App Expr Expr
+    | Lam Name Expr
+    deriving (Eq, Ord, Show, Generic)
+    deriving anyclass Print
+
+data ParseError
+    = NotFunction Atom [Term]
+    | UnknownIdentifier Atom
+    deriving (Show, Generic)
+    deriving anyclass Print
+
+--instance Show ParseError where
+--    show (NotFunction atom args) = show . B.buildText $ do
+--        "atom" >> show atom >> " is a value, cannot apply " >> foldr (\x accum -> B.text x >> " " >> accum) (B.text "") args >> "to it."
+
+-- | This function only read scope and maps `Atom` to `Expr` node
+processAtom :: Scope -> Atom -> Either ParseError Expr
+processAtom _s int@Int{}    = Right (Lit int)
+processAtom _s str@String{} = Right (Lit str)
+processAtom _s Nil          = Right (Lit Nil)
+processAtom s ident@(Ident str) = case M.lookup str s of
+    Just value -> Right value
+    Nothing -> Left (UnknownIdentifier ident)
+
+-- | Make nested list become canonical, say, it maps `List [List [Atom Nil]]` to `Atom Nil`
+flattenTermList :: Term -> Term
+flattenTermList a@Atom{} = a
+flattenTermList (List []) = Atom Nil -- this is unreachable because tokeniser already deal with this
+flattenTermList (List terms@(term:rest)) = case length terms of
+    0 -> Atom Nil -- also unreachable
+    1 -> flattenTermList term
+    _ -> List $ term : (flattenTermList <$> rest) -- Resemble to the Applicative?
+
+defaultScope :: FlatMap k v
+defaultScope = M.empty
+
+processTerm :: Scope -> Term -> Either ParseError (Scope, Expr)
+processTerm s term = processTerm_ s (flattenTermList term)
+
+-- | Process term without flatten the nested list
+processTerm_ :: Scope -> Term -> Either ParseError (Scope, Expr)
+processTerm_ s (Atom a) = case processAtom s a of
+    Left err -> Left err
+    Right expr -> Right (s, expr)
+
+{-
+processTerm_ s (List (term:rest)) = do
+    case processTerm_ s term of
+        Left err -> Left err
+        Right (s, expr) -> if rest == []
+            then Right (s, expr)
+            else Right (s, App expr (processTerm_ s rest)) -- foldr
+-}
+
+-- processTerm defaultScope $ flattenTermList $ fromRight (Atom Nil) $ P.parse' sexp "(a)"
--- src/Tokeniser.hs
+++ src/Tokeniser.hs
@@ -1,13 +1,11 @@
-{-# language MagicHash #-}
 {-# language BangPatterns #-}
 {-# language DeriveGeneric #-}
 {-# language DeriveAnyClass #-}
 {-# language DerivingStrategies #-}
-{-# language GeneralizedNewtypeDeriving #-}
 {-# language OverloadedStrings #-}
 
 module Tokeniser (
-    sexp
+    sexp, Atom(..), Term(..)
 ) where
 
 import Data.Functor (($>))
@@ -15,20 +13,23 @@ import GHC.Generics (Generic)
 import GHC.Word (Word8)
 
 import Z.Data.Text                   (Text, Print)
-import Z.Data.Vector                 (Vector)
 import qualified Z.Data.Text.Base    as T
 import qualified Z.Data.Parser       as P
-import qualified Z.Data.Vector       as V
 import Z.Data.JSON.Value (string)
 
 import Z.Data.ASCII
 
-data SExpr
+data Atom
     = Ident  Text     -- | Atom, identifier, potentially keyword
     | Int    Integer  -- | Atom, number literal
     | String Text     -- | Atom, string literal
     | Nil             -- | empty
-    | List   (Vector SExpr)
+    deriving (Eq, Ord, Show, Generic)
+    deriving anyclass Print
+
+data Term
+    = Atom    Atom
+    | List    [Term]
     deriving (Eq, Ord, Show, Generic)
     deriving anyclass Print
 
@@ -45,40 +46,40 @@ ws w = w == TAB || w == SPACE || w == NEWLINE || w == CARRIAGE_RETURN
 {-# inline ws #-}
 
 -- | parse S expression with leading left parenthesis
-sexp :: P.Parser SExpr
+sexp :: P.Parser Term
 sexp = P.word8 PAREN_LEFT *> sexp_
     
-sexp_ :: P.Parser SExpr
+sexp_ :: P.Parser Term
 sexp_ = do
     skipSpaces
     w <- P.peek
     if w == PAREN_RIGHT
-        then P.skipWord8 $> Nil
-        else List <$> loop [] 1
+        then P.skipWord8 $> Atom Nil
+        else List <$> loop []
   where
-    loop :: [SExpr] -> Int -> P.Parser (Vector SExpr)
-    loop acc !n = do
+    loop :: [Term] -> P.Parser [Term]
+    loop acc = do
         !a <- atom
         let acc' = a:acc
         ch <- P.satisfy $ \w -> w == PAREN_RIGHT || ws w
         if ch /= SPACE
-            then pure $! V.packRN n acc'  -- it is PAREN_RIGHT, packing this vector
+            then pure $! reverse acc'  -- it is PAREN_RIGHT, packing this vector
             else do
                 skipSpaces
                 w <- P.peek
                 if w == PAREN_RIGHT
-                    then pure $! V.packRN n acc' -- it is also PAREN_RIGHT
-                    else loop acc' (n+1)
+                    then pure $! reverse acc' -- it is also PAREN_RIGHT
+                    else loop acc'
 
-atom :: P.Parser SExpr
+atom :: P.Parser Term
 atom = do
     skipSpaces
     w <- P.peek
     case w of
-        DOUBLE_QUOTE           -> String <$> string
+        DOUBLE_QUOTE           -> Atom . String <$> string
         PAREN_LEFT             -> P.skipWord8 *> sexp_ 
-        _ | w >= 48 && w <= 57 -> Int <$> P.integer
-          | not (ws w)         -> Ident <$> identifier
+        _ | w >= 48 && w <= 57 -> Atom . Int <$> P.integer
+          | not (ws w)         -> Atom . Ident <$> identifier
           | otherwise          -> P.fail' "unknown token"
 
 identifier :: P.Parser Text
