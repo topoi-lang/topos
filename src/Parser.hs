a58e486ea8179702ab8eecfa192eb15ada978963
zypeh <zypeh.geek@gmail.com>
2021-06-12 21:37:20 +0800

    WIP Parser Program AST

--- src/Parser.hs
+++ src/Parser.hs
@@ -14,65 +14,47 @@ import qualified Z.Data.Builder as B
 
 import Tokeniser
     ( Atom (Ident, Int, String, Nil)
-    , Term (Atom, List)
+    , AbstSynTree (Atom, List)
     )
 
 type Name = Text
 type Scope = FlatMap Name Expr
 
--- SKI combinator
+data Declaration
+    = VarDecl Name Expr
+    | FunDecl Name [Name] Expr
+    deriving (Eq, Show, Generic)
+
 data Expr
     = Var Name
-    | Lit Atom
     | App Expr Expr
     | Lam Name Expr
+    | Lit Literal
+    deriving (Eq, Show, Generic)
+
+data Literal = Int Integer | String Text
     deriving (Eq, Ord, Show, Generic)
     deriving anyclass Print
 
+data Statement = Statement
+
+data Program =
+    Program
+        [Declaration] -- constant and variable declaration
+        [Expr]        -- Body
+        Scope         -- Context for substitution
+        [Statement]   -- Control Flow
+
 data ParseError
-    = NotFunction Atom [Term]
-    | UnknownIdentifier Atom
-    deriving (Show, Generic)
-    deriving anyclass Print
+    = UnableToApply
+    deriving (Show)
 
---instance Show ParseError where
---    show (NotFunction atom args) = show . B.buildText $ do
---        "atom" >> show atom >> " is a value, cannot apply " >> foldr (\x accum -> B.text x >> " " >> accum) (B.text "") args >> "to it."
+parse :: [AbstSynTree] -> Either ParseError Program
+parse [] = Right (Program [] [] M.empty [])
+parse (first:rest) = case first of
 
--- | This function only read scope and maps `Atom` to `Expr` node
-processAtom :: Scope -> Atom -> Either ParseError Expr
-processAtom _s int@Int{}    = Right (Lit int)
-processAtom _s str@String{} = Right (Lit str)
-processAtom _s Nil          = Right (Lit Nil)
-processAtom s ident@(Ident name) = case M.lookup name s of
-    Just expr -> Right expr
-    Nothing -> Left (UnknownIdentifier ident)
+    Atom (Ident "let")
+        | [name, e] <- rest -> do
+            Right (Program [] [] M.empty [])
 
--- | Make nested list become canonical, say, it maps `List [List [Atom Nil]]` to `Atom Nil`
-flattenTermList :: Term -> Term
-flattenTermList a@Atom{} = a
-flattenTermList (List []) = Atom Nil
-flattenTermList (List term:rest) = List $ (flattenTermList term) : (flattenTermList <$> rest)
-
-defaultScope :: FlatMap k v
-defaultScope = M.empty
-
-processTerm :: Scope -> Term -> Either ParseError (Scope, Expr)
-processTerm s term = processTerm_ s (flattenTermList term)
-
--- | Process term without flatten the nested list
-processTerm_ :: Scope -> Term -> Either ParseError (Scope, Expr)
-processTerm_ s (Atom a) = case processAtom s a of
-    Left err -> Left err
-    Right expr -> Right (s, expr)
-
-{-
-processTerm_ s (List (term:rest)) = do
-    case processTerm_ s term of
-        Left err -> Left err
-        Right (s, expr) -> if rest == []
-            then Right (s, expr)
-            else Right (s, App expr (processTerm_ s rest)) -- foldr
--}
-
--- processTerm defaultScope $ flattenTermList $ fromRight (Atom Nil) $ P.parse' sexp "(a)"
+    _ -> undefined
